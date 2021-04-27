49607705ca65cc05022545862cc9c06fd223a48f
oereo <dlstpgns0406@gmail.com>
2021-04-27 16:58:18 +0900

    style: fix line format

--- lotto.py
+++ lotto.py
@@ -9,28 +9,17 @@ import random
 
 lotto_numbers = []
 
-
-"""
-for _ in range(6):
-    while True:
-        lotto_number = random.randint(1,45)
-        if lotto_number not in lotto_numbers:
-            lotto_numbers.append(lotto_number)
-            break
-print(lotto_numbers)
-"""
-
-lotto_prize = {1:5000000, 2:100000, 3:20000, 4:5000, 5:3000, 6: 1000}
-lotto_score = {1:6, 2:5,3:4, 4:3, 5:2, 6:1}
+lotto_prize = {1: 5000000, 2: 100000, 3: 20000, 4: 5000, 5: 3000, 6: 1000}
+lotto_score = {1: 6, 2: 5, 3: 4, 4: 3, 5: 2, 6: 1}
 
 
 def lotto_check(past_lotto_number):
-    #로또 1장을 구매했을 때 얻는 수익 / 랜덤함수를 이용해서 여기서 당첨금이 있을 수도 있고, 없을 수도 있음(=0)
+    # 로또 1장을 구매했을 때 얻는 수익 / 랜덤함수를 이용해서 여기서 당첨금이 있을 수도 있고, 없을 수도 있음(=0)
     one_lotto = []
     for i in range(6):
         while True:
-            output = random.randint(1,45)
-            if output not in one_lotto :
+            output = random.randint(1, 45)
+            if output not in one_lotto:
                 one_lotto.append(output)
                 break
     print(one_lotto)
@@ -39,28 +28,26 @@ def lotto_check(past_lotto_number):
     for i in range(6):
         if one_lotto[i] in past_lotto_number:
             win_count += 1
-    
+
     if win_count < 1:
         print("less than 1 matched: wrong!")
         return 0
-    else :
-        print("you matched more than 1: matched " +str(win_count))
+    else:
+        print("you matched more than 1: matched " + str(win_count))
         score = lotto_score[win_count]
-        print("you got "+str(score)+"rd(th) score")
+        print("you got " + str(score) + "rd(th) score")
         one_prize = lotto_prize[score]
-        print("you got "+str(one_prize)+ "dollor")
-        
+        print("you got " + str(one_prize) + "dollor")
+
         return one_prize
-                
-            
-    
+
 
 def run():
     past_lotto_number = []
     for i in range(6):
         past_lotto_number.append(int(input("지난 주 lotto 번호를 입력해 주세요 : ")))
 
-    money=int(input("구입 금액을 입력하세요: "))
+    money = int(input("구입 금액을 입력하세요: "))
     lotto_count = money // 1000
     benefit_rate = 0
     prize = 0
@@ -73,10 +60,9 @@ def run():
     print("수익률: " + str(benefit_rate))
 
 
-
-
 def main():
     run()
+
 
 if __name__ == "__main__":
     main()
